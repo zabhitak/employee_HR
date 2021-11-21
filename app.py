c9fd0cdbf7c1b8b40bbf1dab2dc261afc30b913d
zabhitak <zabhi1292@gmail.com>
2021-11-21 14:00:20 +0530

    commit

--- app.py
+++ app.py
@@ -1,41 +1,29 @@
 from flask import Flask, render_template, request
-import jsonify
-import requests
 import pickle
-import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
+
 model = pickle.load(open('decisionTree.pkl', 'rb'))
+
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    Source_Type=1
-    Edu_Type=1
     if request.method == 'POST':
-        # satisfaction_level = float(request.form['Satisfaction Level'])
-        # last_evaluation = float(request.form['Last Evaluation'])
-        # number_project = int(request.form['Number of Projects'])
-        # average_montly_hours = int(request.form['Average Monthly Hours'])
-        # time_spend_company = int(request.form['Time Spend Company'])
-        # Work_accident = int(request.form['Work Accident'])
-        # promotion_last_5years = int(request.form['Promotion Last 5 years'])
-        # Departments = int(request.form['Department'])
-        # salary = int(request.form['Salary'])
 
-        a = float(request.form['Satisfaction Level'])
-        b = float(request.form['Last Evaluation'])
-        c = int(request.form['Number of Projects'])
-        d = int(request.form['Average Monthly Hours'])
-        e = int(request.form['Time Spend Company'])
-        f = int(request.form['Work Accident'])
-        g = int(request.form['Promotion Last 5 years'])
-        h = int(request.form['Department'])
-        i = int(request.form['Salary'])
-  
+        a = float(request.form['a'])
+        b = float(request.form['b'])
+        c = int(request.form['c'])
+        d = int(request.form['d'])
+        e = int(request.form['e'])
+        f = int(request.form['f'])
+        g = int(request.form['g'])
+        h = int(request.form['h'])
+        i = int(request.form['i'])
+
         d = {'satisfaction_level': [a], 'last_evaluation': [b],'number_project': [c], 'average_montly_hours': [d],'time_spend_company': [e],'Work_accident': [f],'promotion_last_5years': [g],'Departments': [h],'salary': [i]}
         de = pd.DataFrame(data=d)
         prediction=model.predict(de)
